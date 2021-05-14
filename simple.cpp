547175d0a27c8dc1cf614eedc0b2f64699a8955c
Ashwin <ashwinramesh.1029@gmail.com>
2021-05-14 17:15:01 +0530

    Added cmake files

--- simple.cpp
+++ simple.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <memory>
+#include "simple.h"
 
 int main()
 {
-	std::cout<<"Sample Program\n";
+	std::cout<<"\n Sample Program\n";
+        {
+        auto spMyClass = std::make_shared<MyClass>(10, 20);
+        auto res = spMyClass->Add();
+        std::cout<<"\n res: "<<res<<"\n";
+        }
+
+        MyClass obj(30, 10);
+        auto res = obj.Add();
+
+        std::cout<<"\n res = "<<res<<"\n";
+
+        auto ob = new MyClass(50, 100);
+        std::cout<<"\n res = "<<ob->Add()<<"\n";
 }
